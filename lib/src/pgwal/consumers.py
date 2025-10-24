bbb29997046ae2b02c925f2b08b20baba8a135cb
Avinash Kumar <avi410vikram@gmail.com>
2025-10-24 12:02:32 +0300

    support context-manager calls for WALConsumer

--- lib/src/pgwal/consumers.py
+++ lib/src/pgwal/consumers.py
@@ -8,7 +8,7 @@ from .interface import Wal2JsonDecodingParams
 
 if TYPE_CHECKING:
     from psycopg2.extras import (
-        ReplicationCursor,
+        LogicalReplicationConnection,
         ReplicationMessage,
     )
 
@@ -21,14 +21,15 @@ class WALConsumer(Callable, metaclass=abc.ABCMeta):
 
     def __init__(
         self,
-        cursor: 'ReplicationCursor',
+        conn: 'LogicalReplicationConnection',
         replication_slot: str,
         replication_opts: Wal2JsonDecodingParams,
         publishers: List = None,
         create_repl_slot: bool = False,
         del_repl_slot: bool = False,
     ):
-        self.cursor = cursor
+        self.conn = conn
+        self.cursor = conn.cursor()
         self.replication_slot = replication_slot
         self.replication_opts = replication_opts
         self.create_repl_slot = create_repl_slot
@@ -61,8 +62,34 @@ class WALConsumer(Callable, metaclass=abc.ABCMeta):
                 options=self.replication_opts,
             )
 
+    def stop_replication(self):
+        """Stop replication stream"""
+        if self.del_repl_slot:
+            self.cursor.drop_replication_slot(self.replication_slot)
+        self.cursor.close()
+        self.conn.close()
+
     def __call__(self, msg: 'ReplicationMessage'):
         """consume WAL stream and publish using provided publishers"""
         for publisher in self.publishers:
             publisher.publish(msg)
         msg.cursor.send_feedback(flush_lsn=msg.data_start)
+
+    def __enter__(self):
+        self.stop_replication()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        if exc_type is not None:
+            logger.error(
+                'Unexpected Error while consuming from WAL stream. replication_slot %s',
+                self.replication_slot,
+                exc_info=(
+                    exc_type,
+                    exc_val,
+                    exc_tb,
+                ),
+            )
+            return False
+        self.stop_replication()
+        return True
