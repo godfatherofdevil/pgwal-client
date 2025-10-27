20f6136269e738b316a272602b3f8c1cff141351
Avinash Kumar <avi410vikram@gmail.com>
2025-10-27 12:16:24 +0200

    refactor and rename interface models

--- lib/src/pgwal/interface.py
+++ lib/src/pgwal/interface.py
@@ -1,23 +1,24 @@
 """Internal interface models module"""
 # pylint: disable=C0103
 from enum import StrEnum
-from pydantic import BaseModel, Field
+from typing import List
+
+from pydantic import BaseModel, Field, field_validator
 
 
-class DecodingParamsActions(StrEnum):
-    """Possible values to wal2json decoding parameter actions"""
+class WALReplicationActions(StrEnum):
+    """Possible values to replication option parameter actions"""
 
     insert = 'insert'
     update = 'update'
     delete = 'delete'
     truncate = 'truncate'
-    all = 'insert, update, delete, truncate'
 
 
-class DecodingParamsValues(StrEnum):
+class WALReplicationValues(StrEnum):
     """
-    Possible values to wal2json decoding parameters,
-    see Wal2JsonDecodingParams for complete list
+    Possible replication option values,
+    see WALReplicationOpts for complete list
     """
 
     false = '0'
@@ -25,102 +26,114 @@ class DecodingParamsValues(StrEnum):
     empty = ''
 
 
-class Wal2JsonDecodingParams(BaseModel):
+class WALReplicationOpts(BaseModel):
     """
-    Supported decoding parameters for wal2json decoding plugin.
+    Supported replication options, see full list below
     https://github.com/eulerto/wal2json/tree/wal2json_2_6?tab=readme-ov-file#parameters
     """
 
-    include_xids: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_xids: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-xids',
     )
-    include_timestamp: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_timestamp: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-timestamp',
     )
-    include_schemas: DecodingParamsValues | None = Field(
-        DecodingParamsValues.true,
+    include_schemas: WALReplicationValues | None = Field(
+        WALReplicationValues.true,
         serialization_alias='include-schemas',
     )
-    include_types: DecodingParamsValues | None = Field(
-        DecodingParamsValues.true,
+    include_types: WALReplicationValues | None = Field(
+        WALReplicationValues.true,
         serialization_alias='include-types',
     )
-    include_typmod: DecodingParamsValues | None = Field(
-        DecodingParamsValues.true,
+    include_typmod: WALReplicationValues | None = Field(
+        WALReplicationValues.true,
         serialization_alias='include-typmod',
     )
-    include_type_oids: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_type_oids: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-type-oids',
     )
-    include_domain_data_type: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_domain_data_type: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-domain-data-type',
     )
-    include_column_positions: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_column_positions: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-column-positions',
     )
-    include_origin: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_origin: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-origin',
     )
-    include_not_null: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_not_null: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-not-null',
     )
-    include_default: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_default: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-default',
     )
-    include_pk: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_pk: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-pk',
     )
-    numeric_data_types_as_string: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    numeric_data_types_as_string: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='numeric-data-types-as-string',
     )
-    pretty_print: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    pretty_print: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='pretty-print',
     )
-    write_in_chunks: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    write_in_chunks: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='write-in-chunks',
     )
-    include_lsn: DecodingParamsValues | None = Field(
-        DecodingParamsValues.false,
+    include_lsn: WALReplicationValues | None = Field(
+        WALReplicationValues.false,
         serialization_alias='include-lsn',
     )
-    include_transaction: DecodingParamsValues | None = Field(
-        DecodingParamsValues.true,
+    include_transaction: WALReplicationValues | None = Field(
+        WALReplicationValues.true,
         serialization_alias='include-transaction',
     )
-    filter_origins: DecodingParamsValues | None = Field(
-        DecodingParamsValues.empty,
+    filter_origins: WALReplicationValues | None = Field(
+        WALReplicationValues.empty,
         serialization_alias='filter-origins',
     )
-    filter_tables: DecodingParamsValues | None = Field(
-        DecodingParamsValues.empty,
+    filter_tables: WALReplicationValues | None = Field(
+        WALReplicationValues.empty,
         serialization_alias='filter-tables',
     )
-    add_tables: DecodingParamsValues | None = Field(
-        DecodingParamsValues.empty,
+    add_tables: WALReplicationValues | None = Field(
+        WALReplicationValues.empty,
         serialization_alias='add-tables',
     )
-    filter_msg_prefixes: DecodingParamsValues | None = Field(
-        DecodingParamsValues.empty,
+    filter_msg_prefixes: WALReplicationValues | None = Field(
+        WALReplicationValues.empty,
         serialization_alias='filter-msg-prefixes',
     )
-    add_msg_prefixes: DecodingParamsValues | None = Field(
-        DecodingParamsValues.empty,
+    add_msg_prefixes: WALReplicationValues | None = Field(
+        WALReplicationValues.empty,
         serialization_alias='add-msg-prefixes',
     )
-    format_version: DecodingParamsValues | None = Field(
-        DecodingParamsValues.true,
+    format_version: WALReplicationValues | None = Field(
+        WALReplicationValues.true,
         serialization_alias='format-version',
     )
-    actions: str | None = Field(DecodingParamsActions.all)
+    actions: List[WALReplicationActions] | [
+        WALReplicationActions.insert,
+        WALReplicationActions.update,
+        WALReplicationActions.delete,
+        WALReplicationActions.truncate,
+    ]
+
+    @field_validator('actions')
+    @classmethod
+    def validate_actions(cls, value: List) -> str:
+        """Validate and build the string format for the actions"""
+        obj = super().model_validate(value)
+        return ', '.join(action.value for action in obj.actions)
